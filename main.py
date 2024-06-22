b759f73dd8eccd799599004edce7b2a8b578a2e3
pereira <gustavosouzapereira4@gmail.com>
2024-06-22 11:52:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+n1 = float(input("Digite um número:"))
+n2 = float(input("Digite outro número:"))
+
+if n1 > n2:
+  print(f"{n1} é o maior")
+
+elif n1==n2:
+  print("Os numeros são iguais")
+
+else:
+  print(f"´{n2} é o maior")
